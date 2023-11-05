3044ca878799b9ec1649d97a77a39f9a01af0400
William Chen <billchen314@gmail.com>
2023-11-04 23:48:03 -0700

    Working on Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import HeaderComponent from './components/HeaderComponent';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.css';
+import Classes from "./Classes";
+import Home from ".";
+
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <Router>
+        <HeaderComponent />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/Classes" element={<Classes />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
