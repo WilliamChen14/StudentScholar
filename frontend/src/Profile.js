719d1a31a1b7877288705f38fd4a13d5de0aa9c3
susanaasun <ss2susana@gmail.com>
2023-12-06 23:36:32 -0800

    Updated the design for the Profile Page

--- frontend/src/Profile.js
+++ frontend/src/Profile.js
@@ -95,7 +95,7 @@ function Notes() {
 
     return (
         <div className="my-notes">
-            <h2> My Pages/Favorited</h2>
+            <p id="title"> My Pages/Favorited</p>
 
             {/* Metadataarray.map -> button component (onC) */}
 
@@ -318,7 +318,7 @@ function Profile() {
       <div className="user-info">
         {isLoggedIn ? (
           <div>
-            <h2>My Profile</h2>
+            <p id="title">My Profile</p>
             <p>Email Address: {usernameText} </p>
             <br />
             <button onClick={logout}>Log out</button>
@@ -331,7 +331,7 @@ function Profile() {
       {isLoggedIn && (
         <div className="my-classes">
           <div className="my-classes-container">
-            <h2>My Classes</h2>
+            <p id="title">My Classes</p>
             <ClassCodeBox addClass={addClass} />
             <ul className="class-list">
               {userClasses.map((cls, index) => (
