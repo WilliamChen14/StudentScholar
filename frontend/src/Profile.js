a389f536e74a56ad2051c34b121c01140de25cbe
susanaasun <ss2susana@gmail.com>
2023-12-06 22:26:52 -0800

    Added a button component to the My Pages Section

--- frontend/src/Profile.js
+++ frontend/src/Profile.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { googleLogout, useGoogleLogin } from '@react-oauth/google';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
+
 
 
 import './Profile.css';
@@ -47,14 +49,22 @@ function ClassCodeBox({ addClass, classNames }) {
   );
 }
 
-function Notes() {
+function Notes({buttonComponent, randomOnClickHandler}) {
     return (
         <div className="my-notes">
             <h2> My Pages/Favorited</h2>
+            <div className="profile-buttons">
+              {buttonComponent({ onClick: randomOnClickHandler, label: "Button 1" })}
+            </div>
         </div>
     );
 }
 
+function Button({onClick, label}) {
+  return (
+    <button onClick={onClick}>{label}</button>
+  );
+}
 function Profile() {
 
   const [user, setUser] = useState(null);
@@ -68,6 +78,9 @@ function Profile() {
   const [classNames, setClassNames] = useState({});
   const [usernameText, setUsernameText] = useState("");
 
+  const randomOnClickHandler = () => {
+    alert('Button clicked!'); 
+  };
 
 
   const login = useGoogleLogin({
@@ -248,11 +261,21 @@ function Profile() {
 
   const classCodeBox = (
     <ClassCodeBox addClass={addClass} classNames={classNames} />
-  ); 
+  );
+
+  const noteButtonComponent = ({ onClick, label }) => (
+    <Link to="/Notes">
+      <Button onClick={onClick} label={label} />
+    </Link>
+  );
+
+  const notebuttons = (
+    <Notes buttonComponent={noteButtonComponent} randomOnClickHandler={randomOnClickHandler} />
+  );
 
   return (
     <div className="container">
-      {isLoggedIn && <Notes />}
+      {isLoggedIn && notebuttons}
       <div className="user-info">
         {isLoggedIn ? (
           <div>
